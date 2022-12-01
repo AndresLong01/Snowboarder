fb14c7380cf9ef88ded2faf796c20f69f9253780
Andres Long <andreslong95@gmail.com>
2022-12-01 17:06:12 -0600

    crash detection changes

--- Assets/Scripts/CrashDetect.cs
+++ Assets/Scripts/CrashDetect.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetect : MonoBehaviour
 {
+  [SerializeField] float timeToReload = 0.5f;
+
   void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.tag == "Player")
+    if (other.tag == "Environment")
     {
-      Debug.Log("Down the Slopes brother");
+      Invoke("ReloadScene", timeToReload);
     }
+  }
+
+  void ReloadScene()
+  {
+      SceneManager.LoadScene(0);
   }
 }
